3b84313a4c7f011e8a588971a125bcf0c27b5cd9
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-03-05 16:30:20 -0500

    added a mouseover event for the nav links set a timeout to stop the event after 500 seconds.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+
+//Crearted a mouseOver event listener 
+
+ const overNav = document.querySelector('nav');
+ console.log(overNav);
+ overNav.addEventListener('mouseover',function(event){
+     //hightlighted mouseenter target
+     event.target.style.color ='#f44e42';
+     event.target.style.fontSize = '3rem';
+     event.target.style.border = '1px solid green';
+     event.target.style.background = 'gray';
+
+     //reset the color after a short delay
+     setTimeout(function(){
+         event.target.style.color = 'blue';
+         event.target.style.background = '';
+         event.target.style.border = '';
+     }, 500);
+     }, false);
+
